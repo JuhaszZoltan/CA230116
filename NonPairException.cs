5a827a1e70d1eebf83388f03e85be6322e84ae80
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-01-16 10:08:51 +0100

    Add project files.

---
+++ NonPairException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA230116
+{
+    internal class NonPairException : Exception
+    {
+        public NonPairException(string? message) : base(message)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using CA230116;
+
+Console.Write("írj be egy páros számot: ");
+try
+{
+    int n;
+    if (int.TryParse(Console.ReadLine(), out n))
+    {
+        if (n % 2 != 0)
+        {
+            throw new NonPairException("ez a szám nem páros!");
+        }
+        Console.WriteLine($"a páros szám duplája: {n * 2}");
+    }
+    Console.WriteLine($"n értéke: {n}");
+}
+catch (NonPairException ex)
+{
+    Console.WriteLine("hiba történt!");
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    throw;
+}
+finally
+{
+    Console.WriteLine("a programból való kilépéshez nyomj ENTER-t!");
+    while (Console.ReadKey(true).Key != ConsoleKey.Enter) ;
+}
+
+Console.WriteLine("vége");
+Console.ReadKey();
